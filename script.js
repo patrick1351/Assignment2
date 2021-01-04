5be8bb39a61581958adf063dbd066aea323e8bbc
patrick1351 <s10194041@connect.np.edu.sg>
2021-01-04 16:59:49 +0800

    Testing buttons for next song

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ $(document).ready(function(){
     var google_api_key = "AIzaSyBkatIeGCQxmAuC_YKY3aKy4Vd-ug5Z04E";
     var url = "https://www.googleapis.com/youtube/v3/search";
     var searchresultNumber = 0;
+    
 
     $("#songSearchButton").on("click", function(e) {
             e.preventDefault();
             rollBg();
-            console.log($("#songSearch").val());
             var songName = $("#songSearch").val();
             var parameter = {
                 part: 'snippet',
@@ -39,64 +39,70 @@ $(document).ready(function(){
                 type: "video",
                 // type: video,
             }
+            console.log($("#songSearch").val());
             console.log(parameter);
+            getSong(searchresultNumber, parameter)
+    })
 
-            getSong(searchresultNumber)
-
-            function getSong(searchresultNumber) {
-                $.getJSON(url, parameter, function(data){
-                    console.log(data);
-                    var videoName = data.items[searchresultNumber].snippet.title
-                    var videoThumbnail = data.items[searchresultNumber0].snippet.thumbnails.high.url
-                    var videoDescription = data.items[searchresultNumber].snippet.description
-                    var videoLinkID = data.items[searchresultNumber].id.videoId
-                    searchResultVid(videoName, videoThumbnail, videoDescription, videoLinkID);
-                })
-            }
-
-            function searchResultVid(videoName, videoThumbnail, videoDescription, videoLinkID) {
-                $("#searchbar").css("display", "none");
-                $("#searchResultDisplay").css("display", "block");
-                $("#searchResult").html(`
-                <div class="col-md-5 videoimg"><img src=${videoThumbnail} alt="Thumbnail"  class="img-fluid img-thumbnail"></div>
-                <div class="col-md-7 d-flex align-items-start flex-column">
-                    <div class="row">
-                        <h1>${videoName}</h1>
-                    </div>
-                    <div class="row">
-                        <h2>Channel: ${videoChannel}<h2>
-                    </div>
-                    <div class="row">
-                      <h2>Published: ${videoPublishedDate}<h2>
-                    </div>
-                    <div class="row mt-auto">
-                      <div class="col-md-3">
-                          <a href="https://www.youtube.com/watch?v=${videoLinkID}"><button class="btn btn-primary btn-lg" id="songSearchButton">Search</button></a>
-                      </div>
-                      <div class="col-md-3">
-                          <button class="btn btn-primary btn-lg" id="returnButton">Return</button>
-                      </div>
-                      <div class="col-md-3">
-                        <button class="btn btn-primary btn-lg" id="nextButton">Next</button>
-                    </div>
-                  </div>
-                </div>
-                `)
-            }
-
-            // fetch("")
-            //  .then(Response => Response.json())
-            //  .then(data => console.log(data))
-        })
-
-    $("#returnButton").on("click", function(e) {
+    $(document).on("click", "#returnButton", function(e) {
         e.preventDefault();
         $("#searchResultDisplay").css("display", "none");
         $("#searchbar").css("display", "block");
     })
 
-    $("$nextButton").on("click", function(e) {
+    $(document).on("click", "#nextButton", function(e) {
         e.preventDefault();
+        console.log(searchresultNumber)
+        searchresultNumber = searchresultNumber + 1;
+        if (searchresultNumber > 20){
+            var searchresultNumber = 0;
+        }
+        console.log(searchresultNumber)
+        getSong(searchresultNumber, parameter)
     })
+
+    function getSong(searchresultNumber, parameter) {
+        console.log("Running getSong")
+        console.log(searchresultNumber)
+        $.getJSON(url, parameter, function(data){
+            console.log(data)
+            var videoName = data.items[searchresultNumber].snippet.title;
+            var videoThumbnail = data.items[searchresultNumber].snippet.thumbnails.high.url;
+            var videoChannel = data.items[searchresultNumber].snippet.channelTitle;
+            var videoLinkID = data.items[searchresultNumber].id.videoId;
+            var videoPublishedDate = data.items[searchresultNumber].snippet.publishedAt;
+            searchResultVid(videoName, videoThumbnail, videoChannel, videoLinkID, videoPublishedDate);
+        })
+    }
+
+    function searchResultVid(videoName, videoThumbnail, videoChannel, videoLinkID, videoPublishedDate) {
+        $("#searchbar").css("display", "none");
+        $("#searchResultDisplay").css("display", "block");
+        $("#searchResult").html(`
+        <div class="col-md-5 videoimg"><img src=${videoThumbnail} alt="Thumbnail"  class="img-fluid img-thumbnail"></div>
+        <div class="col-md-7 d-flex align-items-start flex-column">
+            <div class="row">
+                <h1>${videoName}</h1>
+            </div>
+            <div class="row">
+                <h2>Channel: ${videoChannel}<h2>
+            </div>
+            <div class="row">
+              <h2>Published: ${videoPublishedDate}<h2>
+            </div>
+            <div class="row mt-auto">
+              <div class="col-md-3">
+                  <a href="https://www.youtube.com/watch?v=${videoLinkID}"><button class="btn btn-primary btn-lg" id="songSearchButton">Search</button></a>
+              </div>
+              <div class="col-md-3">
+                  <button class="btn btn-primary btn-lg" id="returnButton">Return</button>
+              </div>
+              <div class="col-md-3">
+                <button class="btn btn-primary btn-lg" id="nextButton">Next</button>
+              </div>
+          </div>
+        </div>
+        `)
+    }
 })
 
